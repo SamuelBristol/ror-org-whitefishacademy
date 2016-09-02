e136d6d663e5d8de285547db30029df90081e42e
Sam Bristol <samuel.d.bristol@gmail.com>
2016-09-02 12:54:48 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825084212) do
+ActiveRecord::Schema.define(version: 20160902124710) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "middle_initial"
+    t.string   "last_name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
+    t.index ["slug"], name: "index_posts_on_slug"
   end
 
 end
