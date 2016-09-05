d3162e118c90a2c0a9c9a148545564aad35f260a
Sam Bristol <samuel.d.bristol@gmail.com>
2016-09-05 19:50:35 +0000

    Redirect invalid urls to root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :posts
   
   root 'home#index'
-
+  
+  match '*path' => redirect('/'), via: :get
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
